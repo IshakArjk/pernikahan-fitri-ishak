63f740c0dde6ff42097782800a24701265be1036
IshakArjk <ishak.abdul.rojak@gmail.com>
2025-06-28 20:31:20 +0700

    Update guest.js

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -248,15 +248,15 @@ export const guest = (() => {
          * @param {string} d 
          * @returns {string}
          */
-        const formatDate = (d) => (new Date(d.replace(' ', 'T') + ':00Z')).toISOString().replace(/[-:]/g, '').split('.').shift();
+        const formatDate = (d) => (new Date(d + ':00Z')).toISOString().replace(/[-:]/g, '').split('.').shift();
 
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Fitri and Ishak',
+            dates: `${formatDate('2025-07-06 10:00')}/${formatDate('2025-07-06 11:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Gedung Budaya Wangisagara Jl. Pasir Garut, RT.01/RW.09 Desa Wangisagara, Kec. Majalaya, Kab. Bandung, Jawa Barat 40382.',
             ctz: config.get('tz'),
         });
 
